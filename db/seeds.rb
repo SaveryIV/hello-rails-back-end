06223beebcbd5af5cd5ffcfa68062e414a182b1b
Agustin Soria <soriaagustin8@gmail.com>
2023-12-19 02:16:56 -0300

    Update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Message.create(content: 'Hello buddy')
+Message.create(content: 'Greetings!')
+Message.create(content: 'Hola amigo!')
+Message.create(content: 'Bonjour!')
+Message.create(content: 'Ciao!')
